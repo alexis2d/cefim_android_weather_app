1bd957d94d81165eac0f4e473caf74c69baa8223
Alexis Duquenet <alexisduquenet@gmail.com>
2023-06-29 15:57:07 +0200

    TP2 part 1 & 2 finished

--- app/src/main/java/fr/alexis2d/weatherapp/MainActivity.java
+++ app/src/main/java/fr/alexis2d/weatherapp/MainActivity.java
@@ -2,13 +2,44 @@ package fr.alexis2d.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewCityName;
+    private TextView mTextViewNoConnexion;
+    private View mButtonFavorite;
+    private View mLayoutContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLayoutContent = findViewById(R.id.layout_content);
+
+        mTextViewCityName = findViewById(R.id.text_view_city_name);
+        Toast.makeText(this, mTextViewCityName.getText(), Toast.LENGTH_SHORT).show();
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        mTextViewNoConnexion = findViewById(R.id.text_no_connexion);
+        mButtonFavorite = findViewById(R.id.button_favorite);
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            mTextViewNoConnexion.setVisibility(View.GONE);
+        } else {
+            mLayoutContent.setVisibility(View.GONE);
+            mButtonFavorite.setVisibility(View.GONE);
+            mTextViewNoConnexion.setVisibility(View.VISIBLE);
+        }
     }
+
 }

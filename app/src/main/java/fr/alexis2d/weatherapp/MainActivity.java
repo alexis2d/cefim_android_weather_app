6902893a5b24fcd21d8c2cc5df0765ed58bbf179
Alexis Duquenet <alexisduquenet@gmail.com>
2023-06-29 16:51:30 +0200

    TP2 part 3 finished

--- app/src/main/java/fr/alexis2d/weatherapp/MainActivity.java
+++ app/src/main/java/fr/alexis2d/weatherapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewCityName;
     private TextView mTextViewNoConnexion;
-    private View mButtonFavorite;
+
+    private Button mButtonFirst;
+    private Button mButtonSecond;
+    private Button mButtonFavorite;
     private View mLayoutContent;
 
     @Override
@@ -23,15 +27,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mLayoutContent = findViewById(R.id.layout_content);
-
         mTextViewCityName = findViewById(R.id.text_view_city_name);
-        Toast.makeText(this, mTextViewCityName.getText(), Toast.LENGTH_SHORT).show();
+        mTextViewNoConnexion = findViewById(R.id.text_no_connexion);
+        mButtonFirst = findViewById(R.id.button_first);
+        mButtonSecond = findViewById(R.id.button_second);
+        mButtonFavorite = findViewById(R.id.button_favorite);
+
+        mButtonSecond.setOnClickListener(v ->  Toast.makeText(this, mButtonSecond.getText(), Toast.LENGTH_SHORT).show());
 
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        mTextViewNoConnexion = findViewById(R.id.text_no_connexion);
-        mButtonFavorite = findViewById(R.id.button_favorite);
 
         if (networkInfo != null && networkInfo.isConnected()) {
             mTextViewNoConnexion.setVisibility(View.GONE);
@@ -39,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
             mLayoutContent.setVisibility(View.GONE);
             mButtonFavorite.setVisibility(View.GONE);
             mTextViewNoConnexion.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onClickButton(View v) {
+        if (((Button) v).getText() == getText(R.string.button_first)) {
+            Toast.makeText(this, getText(R.string.button_first), Toast.LENGTH_SHORT).show();
+        } else if (((Button) v).getText() == getText(R.string.button_favorite)){
+            Toast.makeText(this, getText(R.string.button_favorite), Toast.LENGTH_SHORT).show();
         }
     }
 
